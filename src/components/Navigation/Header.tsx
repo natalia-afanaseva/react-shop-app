2e3e25786ff7e0dccfd21a222d5763a34b36776f
Natalia A <justnatasha1@gmail.com>
2022-09-03 14:16:23 +0300

    added sorting on Orders page and made adjustments to navbar on small devices

--- src/components/Navigation/Header.tsx
+++ src/components/Navigation/Header.tsx
@@ -19,43 +19,60 @@ const Header: React.FC = () => {
   }, [dispatch]);
 
   return (
-    <header className="navbar navbar-expand-lg px-5">
-      <Link to="/">
-        <h1>Cosmetique</h1>
-      </Link>
-
-      <nav className="collapse navbar-collapse justify-content-end">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <button
-              className="nav-link"
-              type="button"
-              data-bs-toggle="offcanvas"
-              data-bs-target="#offcanvasRight"
-              aria-controls="offcanvasRight"
-            >
-              Checkout
-            </button>
-          </li>
-          {isAuth ? (
-            <li className="nav-item">
-              <Link
-                to="/orders"
-                className={`nav-link ${
-                  location.pathname === "/orders" ? "fw-bold" : ""
-                }`}
-              >
-                Orders
-              </Link>
-            </li>
-          ) : (
-            <li className="nav-item">
-              <button className="nav-link" onClick={handleSignIn}>
-                Login
-              </button>
-            </li>
-          )}
-        </ul>
+    <header>
+      <nav className="navbar navbar-expand-lg bg-light">
+        <div className="container-fluid">
+          <Link to="/">
+            <h1>Cosmetique</h1>
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse justify-content-lg-end"
+            id="navbarSupportedContent"
+          >
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <button
+                  className="nav-link"
+                  type="button"
+                  data-bs-toggle="offcanvas"
+                  data-bs-target="#offcanvasRight"
+                  aria-controls="offcanvasRight"
+                >
+                  Checkout
+                </button>
+              </li>
+              {isAuth ? (
+                <li className="nav-item">
+                  <Link
+                    to="/orders"
+                    className={`nav-link text-center text-lg-start ${
+                      location.pathname === "/orders" ? "fw-bold" : ""
+                    }`}
+                  >
+                    Orders
+                  </Link>
+                </li>
+              ) : (
+                <li className="nav-item">
+                  <button className="nav-link" onClick={handleSignIn}>
+                    Login
+                  </button>
+                </li>
+              )}
+            </ul>
+          </div>
+        </div>
       </nav>
     </header>
   );
--- src/views/UserOrders.tsx
+++ src/views/UserOrders.tsx
@@ -1,4 +1,4 @@
-import React, { memo, useEffect, useState } from "react";
+import React, { memo, useEffect, useState, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAppSelector } from "../redux/hooks";
 import { SingleOrder } from "../types/data";
@@ -13,6 +13,7 @@ const UserOrders: React.FC = () => {
 
   const [orders, setOrders] = useState<SingleOrder[] | undefined>(undefined);
   const [loading, setLoading] = useState(false);
+  const [activeSort, setActiveSort] = useState("");
 
   useEffect(() => {
     if (!currentUserUid) {
@@ -35,6 +36,44 @@ const UserOrders: React.FC = () => {
       .finally(() => setLoading(false));
   }, [currentUserUid, navigate]);
 
+  const handleSort = useCallback(
+    (type: "date" | "status") => {
+      switch (type) {
+        case "date":
+          if (orders && orders?.length > 0) {
+            setActiveSort("date");
+            const copied = [...orders];
+            copied.sort(function sortByDate(a, b) {
+              return b.created.seconds - a.created.seconds;
+            });
+            setOrders(copied);
+          }
+          break;
+
+        case "status":
+          if (orders && orders?.length > 0) {
+            setActiveSort("status");
+            const copied = [...orders];
+            copied.sort(function sortByStatus(a, b) {
+              if (a.status < b.status) {
+                return -1;
+              }
+              if (a.status > b.status) {
+                return 1;
+              }
+              return 0;
+            });
+            setOrders(copied);
+          }
+          break;
+
+        default:
+          break;
+      }
+    },
+    [orders]
+  );
+
   return (
     <div className="row py-5 justify-content-center orders">
       <div className="col-12 col-lg-8">
@@ -43,16 +82,43 @@ const UserOrders: React.FC = () => {
           <Loader />
         ) : (
           <>
-            {orders?.map((order) => (
-              <UsersOrderWrapper
-                id={order.id}
-                created={order.created}
-                products={order.products}
-                status={order.status}
-                totalSum={order.totalSum}
-                key={order.id}
-              />
-            ))}
+            {orders && orders.length > 0 ? (
+              <>
+                <div className="d-flex align-items-center flex-column flex-lg-row">
+                  <h6 className="col-12 col-lg-8">Sort by: </h6>
+                  <button
+                    type="button"
+                    className={`col-12 col-lg-2 ${
+                      activeSort === "date" ? "text-decoration-underline" : ""
+                    }`}
+                    onClick={() => handleSort("date")}
+                  >
+                    Order date
+                  </button>
+                  <button
+                    type="button"
+                    className={`col-12 col-lg-2 ${
+                      activeSort === "status" ? "text-decoration-underline" : ""
+                    }`}
+                    onClick={() => handleSort("status")}
+                  >
+                    Status
+                  </button>
+                </div>
+                {orders?.map((order) => (
+                  <UsersOrderWrapper
+                    id={order.id}
+                    created={order.created}
+                    products={order.products}
+                    status={order.status}
+                    totalSum={order.totalSum}
+                    key={order.id}
+                  />
+                ))}
+              </>
+            ) : (
+              <h6>No orders yet.</h6>
+            )}
           </>
         )}
       </div>
